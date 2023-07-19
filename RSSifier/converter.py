c857150e31fe4cf237d3d31fcec4fdf6bc80df7d
Mason Logan <masonlogan1@gmail.com>
2023-07-19 00:43:45 -0400

    refactor converter to use file-like objects rather than filenames; add flask infrastructure

--- RSSifier/converter.py
+++ RSSifier/converter.py
@@ -2,40 +2,36 @@ import csv
 import re
 
 DEFAULT_PATTERN = 'https://www.tumblr.com/[A-Za-z--]*'
-IGNORED_URL_TYPES = ('reblog', 'live', 'blog', 'dashboard', 'explore', 'inbox',
-                     'new', 'reblog', 'settings', '')
+NONUSER_TYPES = ('reblog', 'live', 'blog', 'dashboard', 'explore', 'inbox',
+                     'new', 'reblog', 'settings', 'about', 'policy')
 
 
-def get_from_file(filename: str, url_pattern: str = DEFAULT_PATTERN,
-                  ignored_urls: str = IGNORED_URL_TYPES) -> list:
-    """Opens the file, finds all tumblr urls, filters out ones we don't want,
-    and returns all of them as a list"""
-    with open(filename, 'r') as reader:
-        # generates all found strings in the file
-        gen_urls = (re.findall(url_pattern, line) for line in
-                    reader.readlines())
-        # unpacks gen_urls into a list of strings; turns into set for uniqueness
-        urls = set(sum([url for url in gen_urls
-                        if url is not None
-                        and url != ''
-                        and url not in ignored_urls],
-                       []))
-    return list(urls)
+def users_from_file(datafile, url_pattern: str = DEFAULT_PATTERN,
+                  ignored_users: str = NONUSER_TYPES):
+    """Opens the file, finds all potential user urls, filters out ones we
+    know are not actually users, and returns all of them as a list"""
+    # generates all tumblr urls in the file
+    gen_urls = (re.findall(url_pattern, line.decode()) for line in datafile.readlines())
+    # unpacks gen_urls into a list of strings; turns into set for uniqueness
+    urls = set(sum([url for url in gen_urls if url is not None], []))
+    # separates the username and filters out things we know are not usernames
+    return (user for user in (url.split('/')[-1] for url in urls)
+            if user not in ignored_users
+            and user != '')
 
 
-def get_rss_urls(urls) -> list:
-    return [f"""https://{url.split('/')[-1]}.tumblr.com/rss""" for url in urls]
+def get_rss_urls(users):
+    return (f"""https://{user}.tumblr.com/rss""" for user in sorted(users))
 
 
-def parse_rss_urls_from_document(filename):
-    return get_rss_urls(get_from_file(filename=filename))
+def parse_rss_urls_from_document(datafile):
+    return get_rss_urls(users_from_file(datafile=datafile))
 
 
-def create_csv_from_urls(urls, destination):
-    with open(destination, 'w+') as writer:
-        csv_writer = csv.writer(writer)
-        for url in urls:
-            csv_writer.writerow([url])
+def create_csv_from_urls(urls, writer):
+    csv_writer = csv.writer(writer)
+    for url in urls:
+        csv_writer.writerow([url])
 
 
 def create_csv_from_file(file_in, destination):
